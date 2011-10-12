3922b60421c6c6dbf21aae580688a2cad6b982e3
Chris Whitworth <chris@rockhopper>
2011-10-12 22:27:22 +0100

    Fix it to maybe make some things work ish
    --HG--
    extra : convert_revision : chris%40rockhopper-20111012212722-mhustxttf19qu8h8

--- CPU/Dispatch.py
+++ CPU/Dispatch.py
@@ -7,19 +7,19 @@ Created on 12 Oct 2011
 class Dispatcher:
     def __init__(self, decoder, addressDispatcher, executionDispatcher):
         self.decoder = decoder
-        self.addressingTable = { "imp", addressDispatcher.implicit,
-                                 "acc", addressDispatcher.accumulator,
-                                 "imm", addressDispatcher.immediate,
-                                 "zp", addressDispatcher.zeroPage,
-                                 "zpx", addressDispatcher.zeroPageX,
-                                 "zpy", addressDispatcher.zeroPageY,
-                                 "rel", addressDispatcher.relative,
-                                 "abs", addressDispatcher.absolute,
-                                 "abx", addressDispatcher.absoluteX,
-                                 "aby", addressDispatcher.absoluteY,
-                                 "ind", addressDispatcher.indirect,
-                                 "inx", addressDispatcher.indirectX,
-                                 "iny", addressDispatcher.indirectY
+        self.addressingTable = { "imp": addressDispatcher.implicit,
+                                 "acc": addressDispatcher.accumulator,
+                                 "imm": addressDispatcher.immediate,
+                                 "zp" : addressDispatcher.zeroPage,
+                                 "zpx": addressDispatcher.zeroPageX,
+                                 "zpy": addressDispatcher.zeroPageY,
+                                 "rel": addressDispatcher.relative,
+                                 "abs": addressDispatcher.absolute,
+                                 "abx": addressDispatcher.absoluteX,
+                                 "aby": addressDispatcher.absoluteY,
+                                 "ind": addressDispatcher.indirect,
+                                 "inx": addressDispatcher.indirectX,
+                                 "iny": addressDispatcher.indirectY
                                }
     
     def addressDecode(self, opcode, data):
@@ -27,3 +27,4 @@ class Dispatcher:
         data = self.addressingTable[addressingMode]()
     
     def dispatch(self, opcode, data):
+        pass
